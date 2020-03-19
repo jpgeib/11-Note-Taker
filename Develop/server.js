91f44c391a574821cb9628eaf9cf02053a518622
James Geib <jpgeib2@gmail.com>
2020-03-19 12:01:37 -0700

    added notes path

--- Develop/server.js
+++ Develop/server.js
@@ -15,7 +15,9 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'notes.html'));
+});
 
 app.use(express.static('public'));
 
