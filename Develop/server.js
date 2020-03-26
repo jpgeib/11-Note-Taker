e58cc2de188994c7300c3d83227f9dba70ef563c
James Geib <jpgeib2@gmail.com>
2020-03-25 21:37:28 -0700

    testing something

--- Develop/server.js
+++ Develop/server.js
@@ -11,8 +11,16 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-require("./routes/api_routes")(app);
-require("./routes/html_routes")(app);
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'notes.html'));
+});
+
+// require("./routes/api_routes")(app);
+// require("./routes/html_routes")(app);
 
 
 app.use(express.static('public'));
